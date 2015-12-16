a39f42d084bf5cae907db46fb63d5629d48adf9b
Heiko Dietze <hdietze@lbl.gov>
2015-12-16 15:45:59 -0800

    Verbose logging of requests (for debugging)
    * related to issue geneontology/noctua#151

--- minerva-server/src/main/java/org/geneontology/minerva/server/LoggingApplicationEventListener.java
+++ minerva-server/src/main/java/org/geneontology/minerva/server/LoggingApplicationEventListener.java
@@ -1,6 +1,17 @@
 package org.geneontology.minerva.server;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map.Entry;
+
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
+import org.apache.tools.ant.filters.StringInputStream;
+import org.glassfish.jersey.server.ContainerRequest;
+import org.glassfish.jersey.server.ExtendedUriInfo;
+import org.glassfish.jersey.server.model.ResourceMethod;
 import org.glassfish.jersey.server.monitoring.ApplicationEvent;
 import org.glassfish.jersey.server.monitoring.ApplicationEventListener;
 import org.glassfish.jersey.server.monitoring.RequestEvent;
@@ -48,8 +59,15 @@ public class LoggingApplicationEventListener implements ApplicationEventListener
 		public void onEvent(RequestEvent event) {
 			switch (event.getType()) {
 			case RESOURCE_METHOD_START:
-				LOG.info("Resource method " + event.getUriInfo().getMatchedResourceMethod().getHttpMethod()
-						+ " started for request " + requestNumber);
+				ExtendedUriInfo uriInfo = event.getUriInfo();
+				ResourceMethod method = uriInfo.getMatchedResourceMethod();
+				ContainerRequest containerRequest = event.getContainerRequest();
+				LOG.info(requestNumber+" Resource method " + method.getHttpMethod() + " started for request " + requestNumber);
+				LOG.info(requestNumber+" Headers: "+ render(containerRequest.getHeaders()));
+				LOG.info(requestNumber+" Path: "+uriInfo.getPath());
+				LOG.info(requestNumber+" PathParameters: "+ render(uriInfo.getPathParameters()));
+				LOG.info(requestNumber+" QueryParameters: "+ render(uriInfo.getQueryParameters()));
+				LOG.info(requestNumber+" Body: "+getBody(containerRequest));
 				break;
 			case FINISHED:
 				LOG.info("Request " + requestNumber + " finished. Processing time "
@@ -61,5 +79,32 @@ public class LoggingApplicationEventListener implements ApplicationEventListener
 			
 		}
 
+	}
+	
+	private static CharSequence getBody(ContainerRequest request) {
+		String body = null;
+		try {
+			body = IOUtils.toString(request.getEntityStream());
+			// reading the stream consumes it, need to re-create it for the real thing
+			request.setEntityStream(new StringInputStream(body)); 
+		} catch (IOException e) {
+			LOG.warn("Couldn't ready body.", e);
+		}
+		return body;
+	}
+	
+	private static CharSequence render(MultivaluedMap<String, String> map) {
+		StringBuilder sb = new StringBuilder();
+		int count = 0;
+		sb.append('[');
+		for (Entry<String, List<String>> entry : map.entrySet()) {
+			if (count > 0) {
+				sb.append(',');
+			}
+			sb.append('{').append(entry.getKey()).append(',').append(entry.getValue()).append('}');
+			count += 1;
+		}
+		sb.append(']');
+		return sb;
 	}
 }
