eb73089a6f44d24f91704be44abeac20ac1468d2
HyeongSik Kim <yy20716@gmail.com>
2017-11-02 14:55:06 -0700

    Extend GPAD generation codes to support negations in the qualifier field of GPAD

--- minerva-converter/src/main/java/org/geneontology/minerva/legacy/sparql/GPADSPARQLExport.java
+++ minerva-converter/src/main/java/org/geneontology/minerva/legacy/sparql/GPADSPARQLExport.java
@@ -34,6 +34,7 @@ import org.apache.jena.sparql.engine.binding.BindingMap;
 import org.apache.jena.vocabulary.OWL;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.log4j.Logger;
+import org.eclipse.jetty.util.log.Log;
 import org.geneontology.minerva.curie.CurieHandler;
 import org.geneontology.minerva.legacy.sparql.GPADData.ConjunctiveExpression;
 import org.geneontology.rules.engine.Explanation;
@@ -43,10 +44,9 @@ import org.semanticweb.owlapi.model.IRI;
 
 import scala.collection.JavaConverters;
 
+/* 	 Note: the example GPAD files are available at this link: http://www.informatics.jax.org/downloads/reports/mgi.gpa.gz */
 public class GPADSPARQLExport {
-
 	private static final Logger LOG = Logger.getLogger(GPADSPARQLExport.class);
-
 	private static final String ND = "http://purl.obolibrary.org/obo/ECO_0000307";
 	private static final String MF = "http://purl.obolibrary.org/obo/GO_0003674";
 	private static final String BP = "http://purl.obolibrary.org/obo/GO_0008150";
@@ -85,27 +85,41 @@ public class GPADSPARQLExport {
 		this.relationShorthandIndex = shorthandIndex;
 	}
 
-	/*
-	 * This is a bit convoluted in order to minimize redundant queries, for performance reasons.
-	 */
+	/* This is a bit convoluted in order to minimize redundant queries, for performance reasons. */
 	public String exportGPAD(WorkingMemory wm) {
+		/* The first step of constructing GPAD records is to construct candidate/basic GPAD records by running gpad-basic.rq. */ 
 		Model model = ModelFactory.createDefaultModel();
 		model.add(JavaConverters.setAsJavaSetConverter(wm.facts()).asJava().stream().map(t -> model.asStatement(Bridge.jenaFromTriple(t))).collect(Collectors.toList()));
 		QueryExecution qe = QueryExecutionFactory.create(mainQuery, model);
 		Set<GPADData> annotations = new HashSet<>();
 		String modelID = model.listResourcesWithProperty(RDF.type, OWL.Ontology).mapWith(r -> curieHandler.getCuri(IRI.create(r.getURI()))).next();
 		ResultSet results = qe.execSelect();
-		Set<BasicGPADData> basicAnnotations = new HashSet<>();
+		Set<BasicGPADData> basicAnnotations = new HashSet<>();		
 		while (results.hasNext()) {
 			QuerySolution qs = results.next();
-			basicAnnotations.add(new BasicGPADData(qs.getResource("pr").asNode(), IRI.create(qs.getResource("pr_type").getURI()), IRI.create(qs.getResource("rel").getURI()), qs.getResource("target").asNode(), IRI.create(qs.getResource("target_type").getURI())));
+			BasicGPADData basicGPADData = new BasicGPADData(qs.getResource("pr").asNode(), IRI.create(qs.getResource("pr_type").getURI()), IRI.create(qs.getResource("rel").getURI()), qs.getResource("target").asNode(), IRI.create(qs.getResource("target_type").getURI()));			
+			
+			/* See whether the query answer contains not-null blank nodes, which are only set if the matching subgraph 
+			 * contains the property ComplementOf.  If we see such cases, we set the operator field as NOT so that NOT value 
+			 * can be printed in GPAD. */ 
+			if (qs.getResource("blank_comp") != null) basicGPADData.setOperator(GPADOperatorStatus.NOT);
+			basicAnnotations.add(basicGPADData);
 		}
 		qe.close();
+		
+		/* The bindings of ?pr_type, ?rel, ?target_type are candidate mappings or values for the final GPAD records 
+		 * (i.e. not every mapping is used for building the final records of GPAD file; many of them are filtered out later).
+		 * The mappings are 
+		 * 		?pr_type: DB Object ID (2nd in GPAD), ?rel: Qualifier(3rd), ?target_type: GO ID(4th) 
+		 * The rest of fields in GPAD are then constructed by joining the candidate mappings with mappings describing evidences and so on.
+		 * If the output of this exporter (i.e. GPAD files) does not contain the values you expect, 
+		 * dump the above "QuerySolution qs" variable and see whether they are included in the dump. */
 		Set<AnnotationExtension> possibleExtensions = possibleExtensions(basicAnnotations, model);
 		Set<Triple> statementsToExplain = new HashSet<>();
 		basicAnnotations.forEach(ba -> statementsToExplain.add(Triple.create(ba.getObjectNode(), NodeFactory.createURI(ba.getQualifier().toString()), ba.getOntologyClassNode())));
 		possibleExtensions.forEach(ae -> statementsToExplain.add(ae.getTriple()));
 		Map<Triple, Set<Explanation>> allExplanations = statementsToExplain.stream().collect(Collectors.toMap(Function.identity(), s -> toJava(wm.explain(Bridge.tripleFromJena(s)))));
+
 		Map<Triple, Set<GPADEvidence>> allEvidences = evidencesForFacts(allExplanations.values().stream().flatMap(es -> es.stream()).flatMap(e -> toJava(e.facts()).stream().map(t -> Bridge.jenaFromTriple(t))).collect(Collectors.toSet()), model, modelID);
 		for (BasicGPADData annotation : basicAnnotations) {
 			for (Explanation explanation : allExplanations.get(Triple.create(annotation.getObjectNode(), NodeFactory.createURI(annotation.getQualifier().toString()), annotation.getOntologyClassNode()))) {
@@ -131,13 +145,17 @@ public class GPADSPARQLExport {
 								}
 							}
 						}
+
 						final boolean rootViolation;
 						if (rootTerms.contains(annotation.getOntologyClass().toString())) {
 							rootViolation = !ND.equals(currentEvidence.getEvidence().toString());
 						} else { rootViolation = false; }
+
 						if (!rootViolation) {
-							annotations.add(new DefaultGPADData(annotation.getObject(), annotation.getQualifier(), annotation.getOntologyClass(), goodExtensions, 
-									reference, currentEvidence.getEvidence(), currentEvidence.getWithOrFrom(), Optional.empty(), currentEvidence.getDate(), "GO_Noctua", currentEvidence.getAnnotations()));	
+							DefaultGPADData defaultGPADData = new DefaultGPADData(annotation.getObject(), annotation.getQualifier(), annotation.getOntologyClass(), goodExtensions, 
+									reference, currentEvidence.getEvidence(), currentEvidence.getWithOrFrom(), Optional.empty(), currentEvidence.getDate(), "GO_Noctua", currentEvidence.getAnnotations());
+							defaultGPADData.setOperator(annotation.getOperator());
+							annotations.add(defaultGPADData);
 						}
 					});
 				}
@@ -146,6 +164,21 @@ public class GPADSPARQLExport {
 		return new GPADRenderer(curieHandler, relationShorthandIndex).renderAll(annotations);
 	}
 
+	/**
+	 * Given a set of triples extracted/generated from the result/answer of query gpad-basic.rq, we find matching evidence subgraphs. 
+	 * In other words, if there are no matching evidence (i.e. no bindings for evidence_type), we discard (basic) GPAD instance.
+	 * 
+	 * The parameter "facts" consists of triples <?subject, ?predicate, ?object> constructed from a binding of ?pr, ?rel, ?target in gpad_basic.rq. 
+	 * (The codes that constructing these triples are executed right before this method is called).
+	 * 
+	 * These triples are then decomposed into values used as the parameters/bindings for objects of the following patterns.
+	 * 		?axiom owl:annotatedSource   ?subject (i.e. ?pr in gpad_basic.rq) 
+	 * 		?axiom owl:annotatedProperty ?predicate (i.e., ?rel in gpad_basic.rq, which denotes qualifier in GPAD) 
+	 * 		?axiom owl:annotatedTarget    ?object (i.e., ?target in gpad_basic.rq)
+	 * 
+	 * If we find the bindings of ?axioms and the values of these bindings have some rdf:type triples, we proceed. (If not, we discard).
+	 * The bindings of the query gpad-relation-evidence-multiple.rq are then used for filling up fields in GPAD records/tuples.
+	 */
 	private Map<Triple, Set<GPADEvidence>> evidencesForFacts(Set<Triple> facts, Model model, String modelID) {
 		Query query = QueryFactory.create(multipleEvidenceQuery);
 		Var subject = Var.alloc("subject");
@@ -220,7 +253,6 @@ public class GPADSPARQLExport {
 	}
 
 	private static class DefaultConjunctiveExpression implements ConjunctiveExpression {
-
 		private final IRI relation;
 		private final IRI filler;
 
@@ -238,7 +270,5 @@ public class GPADSPARQLExport {
 		public IRI getFiller() {
 			return filler;
 		}
-
 	}
-
 }
