1c3f5a6062e18633614ae797e1b9308654d6cfff
goodb <ben@akamai.bio>
2019-06-25 16:26:13 -0700

    service working with all model query to local blazegraph

--- minerva-server/src/main/java/org/geneontology/minerva/server/handler/ModelSearchHandler.java
+++ minerva-server/src/main/java/org/geneontology/minerva/server/handler/ModelSearchHandler.java
@@ -3,6 +3,11 @@
  */
 package org.geneontology.minerva.server.handler;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
@@ -12,10 +17,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.commons.io.IOUtils;
 import org.geneontology.minerva.BlazegraphMolecularModelManager;
+import org.openrdf.query.BindingSet;
 import org.openrdf.query.MalformedQueryException;
 import org.openrdf.query.QueryEvaluationException;
 import org.openrdf.query.QueryResult;
+import org.openrdf.query.TupleQueryResult;
 import org.openrdf.repository.RepositoryException;
 
 /**
@@ -35,19 +43,53 @@ public class ModelSearchHandler {
         this.timeout = timeout;
 	}
 	
-	public class Result {
-	    private Integer id;
-	    private String name;
+	public class ModelSearchResult {
+	    private Integer n;
+	    private Set<ModelMeta> models;
 	}
+	
+	public class ModelMeta{
+		private String id;
+		private String date;
+		private String title;
+		private String state;
+		private String contributors;
+		
+		public ModelMeta(String id, String date, String title, String state, String contributors) {
+			this.id = id;
+			this.date = date;
+			this.title = title;
+			this.state = state;
+			this.contributors = contributors;
+		}
+	}
+	
 	
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Result searchGet(@QueryParam("query") String queryText)  {
-       // return m3.executeModelSearch(queryText, timeout);
+    public ModelSearchResult searchGet(@QueryParam("query") String queryText) throws MalformedQueryException, QueryEvaluationException, RepositoryException, IOException  {
+    	ModelSearchResult r = new ModelSearchResult();
+    	Set<ModelMeta> models = new HashSet<ModelMeta>();
+    	String sparql = IOUtils.toString(ModelSearchHandler.class.getResourceAsStream("/GetAllModels.rq"), StandardCharsets.UTF_8);
+		//?id ?date ?title ?state (GROUP_CONCAT(?contributor;separator=";") AS ?contributors
+    	TupleQueryResult result = (TupleQueryResult) m3.executeSPARQLQuery(sparql, 10);
+    	int n_models = 0;
+    	while(result.hasNext()) {
+    		BindingSet bs = result.next();
+    		String id = bs.getBinding("id").getValue().stringValue();
+    		String date = bs.getBinding("date").getValue().stringValue();
+    		String title = bs.getBinding("title").getValue().stringValue();
+    		String state = bs.getBinding("state").getValue().stringValue();
+    		String contributors = bs.getBinding("date").getValue().stringValue();
+    		ModelMeta mm = new ModelMeta(id, date, title, state, contributors);
+    		models.add(mm);
+    		n_models++;
+    	}
+    	r.n = n_models;
+    	r.models = models;
+    	result.close();
     //test
     //http://127.0.0.1:6800/modelsearch/?query=bla
-    	Result r = new Result();
-    	r.id = 9; r.name = "fred";
     	return r;
     }
 
