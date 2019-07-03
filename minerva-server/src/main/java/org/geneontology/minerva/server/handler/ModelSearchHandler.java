be34ffbb9ecf663b24640bf60ae5bafeece6adc6
goodb <ben@akamai.bio>
2019-07-03 12:19:52 -0700

    added group query and return element
    still leaving as union in the query.

--- minerva-server/src/main/java/org/geneontology/minerva/server/handler/ModelSearchHandler.java
+++ minerva-server/src/main/java/org/geneontology/minerva/server/handler/ModelSearchHandler.java
@@ -60,14 +60,16 @@ public class ModelSearchHandler {
 		private String title;
 		private String state;
 		private Set<String> contributors;
+		private Set<String> groups;
 		private HashMap<String, Set<String>> query_match;
 
-		public ModelMeta(String id, String date, String title, String state, Set<String> contributors) {
+		public ModelMeta(String id, String date, String title, String state, Set<String> contributors, Set<String> groups) {
 			this.id = id;
 			this.date = date;
 			this.title = title;
 			this.state = state;
 			this.contributors = contributors;
+			this.groups = groups;
 			query_match = new HashMap<String, Set<String>>();
 		}
 	}
@@ -80,14 +82,18 @@ public class ModelSearchHandler {
 			@QueryParam("goterm") Set<String> goterms,
 			@QueryParam("pmid") Set<String> pmids,
 			@QueryParam("title") String title,
-			@QueryParam("state") Set<String> state
+			@QueryParam("state") Set<String> state,
+			@QueryParam("contributor") Set<String> contributor,
+			@QueryParam("group") Set<String> group
 			) throws MalformedQueryException, QueryEvaluationException, RepositoryException, IOException  {
 		if(gene_product_class_uris!=null
 				||goterms!=null
 				||pmids!=null
 				||title!=null
-				||state!=null) {
-			return search(gene_product_class_uris, goterms, pmids, title, state);
+				||state!=null
+				||contributor!=null
+				||group!=null) {
+			return search(gene_product_class_uris, goterms, pmids, title, state, contributor, group);
 		}else {
 			return getAll();
 		}
@@ -101,7 +107,9 @@ public class ModelSearchHandler {
 	//&title=mouse
 	//&pmid=PMID:19911006
 	//&state=development&state=review {development, production, closed, review, delete} or operator
-	public ModelSearchResult search(Set<String> gene_product_class_uris, Set<String> goterms, Set<String>pmids, String title_search,Set<String> state_search) throws MalformedQueryException, QueryEvaluationException, RepositoryException, IOException  {
+	public ModelSearchResult search(
+			Set<String> gene_product_class_uris, Set<String> goterms, Set<String>pmids, 
+			String title_search,Set<String> state_search, Set<String> contributor_search, Set<String> group_search) throws MalformedQueryException, QueryEvaluationException, RepositoryException, IOException  {
 		Set<String> type_uris = new HashSet<String>();
 		if(gene_product_class_uris!=null) {
 			type_uris.addAll(gene_product_class_uris);
@@ -149,11 +157,39 @@ public class ModelSearchHandler {
 			// FILTER (?state IN ("production", , "development", "review", "closed", "delete" ))
 			state_search_constraint = "FILTER (?state IN ("+allowed_states+")) . \n";
 		}
+		String contributor_search_constraint = "";
+		if(contributor_search!=null&&contributor_search.size()>0) {
+			String allowed_contributors = "";
+			int c = 0;
+			for(String contributor : contributor_search) {
+				c++;
+				allowed_contributors+="\""+contributor+"\"";
+				if(c<contributor_search.size()) {
+					allowed_contributors+=",";
+				}
+			}
+			contributor_search_constraint = "FILTER (?contributor IN ("+allowed_contributors+")) . \n";
+		}
+		String group_search_constraint = "";
+		if(group_search!=null&&group_search.size()>0) {
+			String allowed_group = "";
+			int c = 0;
+			for(String group : group_search) {
+				c++;
+				allowed_group+="\""+group+"\"";
+				if(c<group_search.size()) {
+					allowed_group+=",";
+				}
+			}
+			contributor_search_constraint = "FILTER (?group IN ("+allowed_group+")) . \n";
+		}
 		sparql = sparql.replaceAll("<ind_return_list>", ind_return_list);
 		sparql = sparql.replaceAll("<types>", types);
 		sparql = sparql.replaceAll("<pmid_constraints>", pmid_constraints);
 		sparql = sparql.replaceAll("<title_constraint>", title_search_constraint);
 		sparql = sparql.replaceAll("<state_constraint>", state_search_constraint);
+		sparql = sparql.replaceAll("<contributor_constraint>", contributor_search_constraint);
+		sparql = sparql.replaceAll("<group_constraint>", group_search_constraint);
 		
 		TupleQueryResult result = (TupleQueryResult) m3.executeSPARQLQuery(sparql, 10);
 		while(result.hasNext()) {
@@ -164,15 +200,22 @@ public class ModelSearchHandler {
 			String title = bs.getBinding("title").getValue().stringValue();
 			String state = bs.getBinding("state").getValue().stringValue();
 			String contribs = bs.getBinding("contributors").getValue().stringValue();
+			String groups_ = bs.getBinding("groups").getValue().stringValue();
 			Set<String> contributors = new HashSet<String>();
 			if(contributors!=null) {
 				for(String c : contribs.split(";")) {
 					contributors.add(c);
 				}
 			}
+			Set<String> groups = new HashSet<String>();
+			if(groups_!=null) {
+				for(String c : groups_.split(";")) {
+					groups.add(c);
+				}
+			}
 			ModelMeta mm = id_model.get(id);
 			if(mm==null) {
-				mm = new ModelMeta(id, date, title, state, contributors);
+				mm = new ModelMeta(id, date, title, state, contributors, groups);
 			}
 			//matching     		
 			for(String ind : ind_return.keySet()) {
@@ -208,13 +251,20 @@ public class ModelSearchHandler {
 			String title = bs.getBinding("title").getValue().stringValue();
 			String state = bs.getBinding("state").getValue().stringValue();
 			String contribs = bs.getBinding("contributors").getValue().stringValue();
+			String groups_ = bs.getBinding("groups").getValue().stringValue();
 			Set<String> contributors = new HashSet<String>();
 			if(contributors!=null) {
 				for(String c : contribs.split(";")) {
 					contributors.add(c);
 				}
 			}
-			ModelMeta mm = new ModelMeta(id, date, title, state, contributors);
+			Set<String> groups = new HashSet<String>();
+			if(groups_!=null) {
+				for(String c : groups_.split(";")) {
+					groups.add(c);
+				}
+			}
+			ModelMeta mm = new ModelMeta(id, date, title, state, contributors, groups);
 			models.add(mm);
 			n_models++;
 		}
